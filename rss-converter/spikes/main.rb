5a08909e855e63b3690029ae955fc822f8272d6b
Marcelo Vargas <marcelo.vargas90@gmail.com>
2014-06-26 15:59:27 -0300

    Adding a test for rss loading from web

---
+++ rss-converter/spikes/main.rb
@@ -0,0 +1,14 @@
+require 'rss'
+require 'open-uri'
+
+url = 'http://webfoundation.org/feed/'
+open(url) do |rss|
+    feed = RSS::Parser.parse(rss)
+    puts "Title: #{feed.channel.title}"
+    feed.items.each do |item|
+        puts "Item: #{item.title}"
+    end
+
+    puts feed.inspect 
+    puts feed.items[0].inspect
+end
