5c283c76779e253763230fe066efbf7dd9907ac4
Marcelo Vargas <marcelo.vargas90@gmail.com>
2014-06-26 15:09:26 -0300

    Adding spec and conversion of more properties for the Article vocabulary

---
+++ rss-converter/lib/turtle.rb
@@ -0,0 +1,40 @@
+require 'rdf'
+require 'rdf/turtle'
+
+class Turtle
+  def initialize(prefixes = {})
+    @prefixes = prefixes
+    @resources = []
+  end
+
+  def add_resource(resource)
+    @resources.push(resource)
+    self
+  end
+
+  def to_string
+    graph = RDF::Graph.new 
+
+    @resources.each do |resource|
+        resourceUri = RDF::URI.new(resource.uri)
+        graph << [resourceUri, RDF.type, RDF::SCHEMA.Article]
+        graph << [resourceUri, RDF::SCHEMA.headline, resource.properties["schema:headline"]]
+        if (resource.properties["schema:url"] != nil)
+            graph << [resourceUri, RDF::SCHEMA.url, RDF::URI.new(resource.properties["schema:url"])]
+        end
+        if (resource.properties["schema:articleSection"] != nil)
+          graph << [resourceUri, RDF::SCHEMA.articleSection, resource.properties["schema:articleSection"]]
+        end
+        if (resource.properties["schema:datePublished"] != nil)
+          graph << [resourceUri, RDF::SCHEMA.datePublished, resource.properties["schema:datePublished"]]
+        end
+    end
+
+    RDF::Turtle::Writer.buffer(prefixes: @prefixes) do |writer|
+        graph.each_statement do |statement|
+            writer << statement
+        end
+    end
+  end
+
+end
---
+++ rss-converter/spec/turtle_spec.rb
@@ -0,0 +1,60 @@
+require 'turtle'
+require 'resource'
+
+describe Turtle do
+
+  prefixes = {
+      rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      schema: 'http://schema.org/'
+  }
+
+  context "given a resource of type schema:Article with title" do
+
+    resource = Resource.new("http://the/link/#thelink", "schema:Article")
+      .add_property("schema:headline", "About the project")
+
+    turtle = Turtle.new(prefixes)
+      .add_resource(resource)
+
+    it "should convert to turtle format" do
+      turtle_as_string = turtle.to_string
+
+      expected =
+"@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix schema: <http://schema.org/> .
+
+<http://the/link/#thelink> a schema:Article;
+   schema:headline \"About the project\" .\n"
+
+      expect(turtle_as_string).to eq(expected)
+    end
+  end
+
+    context "given a resource of schema:Article with more than one property" do
+
+      resource = Resource.new("http://the/link/#thelink", "schema:Article")
+        .add_property("schema:url", "http://webfoundation.org/2014/06/a-message-of-congratulations/")
+        .add_property("schema:headline", "A message of congratulations…")
+        .add_property("schema:articleSection", "General")
+        .add_property("schema:datePublished", "Mon, 23 Jun 2014 10:59:34 +0000")
+
+      turtle = Turtle.new(prefixes)
+        .add_resource(resource)
+
+      it "should convert to turtle format" do
+        turtle_as_string = turtle.to_string
+
+        expected =
+  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix schema: <http://schema.org/> .
+
+<http://the/link/#thelink> a schema:Article;
+   schema:articleSection \"General\";
+   schema:datePublished \"Mon, 23 Jun 2014 10:59:34 +0000\";
+   schema:headline \"A message of congratulations…\";
+   schema:url <http://webfoundation.org/2014/06/a-message-of-congratulations/> .\n"
+
+        expect(turtle_as_string).to eq(expected)
+      end
+    end
+  end
