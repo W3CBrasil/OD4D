a5bea91c906fb04f4094e2cf0c0df4b507bc31f7
Rafael Magrin <rafael.magrin@gmail.com>
2014-06-27 10:22:48 -0300

    Adding Fuubar gem to improve progress bar when running rspec tests for rss-converter and added curl, vim and git to bootstrap script.

--- system-configuration/bootstrap.sh
+++ system-configuration/bootstrap.sh
@@ -16,9 +16,11 @@ sudo apt-get update -y
 # this is very slow, we are trying to figure out a better way to do this
 # unattended-upgrades
 
+sudo apt-get install -y vim curl git
+
 $PROJECT_FOLDER/od4d/bootstrap.sh
 $PROJECT_FOLDER/rss-converter/bootstrap.sh
 $PROJECT_FOLDER/semantic-repo/bootstrap.sh
 
 # remove uneeded packages
-sudo apt-get autoremove
+sudo apt-get autoremove -y
